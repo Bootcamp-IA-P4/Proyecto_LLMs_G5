66aa307fc103a631dcabd7afcda6ccb3212e3321
Nho89 <noa1720@gmail.com>
2025-07-17 11:17:03 +0200

    feat(generators): added testing Ollama

---
+++ server/generators/text.py
@@ -0,0 +1,13 @@
+from langchain_ollama import OllamaLLM
+
+# Ahora vamos a crear una instancia del modelo que vamos a usar.
+# llm = OllamaLLM(model="llama3")
+
+# Vamos a crear un prompt para probar, más adelante lo haremss en la carpeta de prompts.
+# prompt = "Escribe un tweet corto y atractivo sobre la llegada del hombre a la Luna, incluyendo hashtags relevantes."
+
+# Ahora vamos invocar al modelo con el prompt.
+# response = llm.invoke(prompt)
+# print(response)
+
+
